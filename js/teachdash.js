2febe6d884e614c6a593a5805fc38c85df81298c
sumit <roy.sumit31@gmail.com>
2021-02-19 13:05:33 +0530

    new page

--- js/teachdash.js
+++ js/teachdash.js
@@ -1,12 +1,3 @@
 $(document).ready(function () {
-  $("#toggle").click(function () {
-    $(".menubar").toggle(
-      function () {
-        $(this).animate({ width: 0 });
-      },
-      function () {
-        $(this).animate({ width: "auto" });
-      }
-    );
-  });
+  
 });
